037ead60ed19ed5856a5780522cecc96c6aa0a73
Unknown <vincent.valant@gmail.com>
2018-10-26 15:51:02 -0400

    Completed data pull for comm charts
    Save point before starting on individual variables

--- src/data/generate_report_data.py
+++ src/data/generate_report_data.py
@@ -30,25 +30,25 @@ def comm_pie_chart(comm_df, comm_pie_chart_data, username, date):
 	return comm_pie_chart_data
 
 
+def comm_days_line_chart(comm_df, comm_days_line_chart_data, username, multi_index):
+	cols = comm_days_line_chart_data.columns
+	user_xs = comm_days_line_chart_data.xs(username)
+	# date_indices = np.unique(comm_days_line_chart_data.index.get_level_values('date'))
+	date_indices = user_xs.index
+	for date in date_indices:
+		if date in comm_df.index:
+			comm_days_line_chart_data.loc[(username, date), cols] = comm_df.loc[date,cols]
+	return comm_days_line_chart_data
+
+
 def comm_vol_line_chart(comm_df, comm_vol_line_chart_data, username, multi_index):
-	# cols = [ 'total_comm_days', 'risky_comm_days', 'supportive_comm_days']
-	# labels = [ 'Any Comm', 'Risky Comm', 'Supportive Comm']
-	# linestyle=['solid', 'dashed', 'dotted']
 	cols = comm_vol_line_chart_data.columns
 	user_xs = comm_vol_line_chart_data.xs(username)
-	# comm_vol_line_chart_data[username] = comm_df.loc[multi_index['date']]
-	date_indices = np.unique(comm_vol_line_chart_data.index.get_level_values('date'))
+	# date_indices = np.unique(comm_vol_line_chart_data.index.get_level_values('date'))
 	date_indices = user_xs.index
-	# print(date_indices)
 	for date in date_indices:
-		print('here')
-		# print(comm_vol_line_chart_data.loc[(username, date), cols])
-		# print(comm_df[date])
 		if date in comm_df.index:
-			# print(comm_df.loc[date, cols])
 			comm_vol_line_chart_data.loc[(username, date), cols] = comm_df.loc[date,cols]
-	# for i, c in enumerate(users[e]['weekly_activity'][cols][:-1]
-	# print(comm_vol_line_chart_data.xs(username))
 	return comm_vol_line_chart_data
 
 # pull individual report variables
@@ -65,8 +65,10 @@ def generate_report_data(usernames, date_indices, PROJ_ROOT):
 
 	comm_pie_chart_cols = ['risky_percent', 'neutral_percent', 'supportive_percent', 'unrated_percent']
 	comm_pie_chart_data = pd.DataFrame(np.nan, index=usernames, columns=comm_pie_chart_cols)
-	comm_vol_line_chart_cols = ['username', 'total_comm_days', 'risky_comm_days', 'supportive_comm_days']
-	comm_vol_line_chart_data = pd.DataFrame(np.nan, index=multi_index, columns=comm_pie_chart_cols)
+	comm_days_line_chart_cols = ['total_comm_days', 'risky_comm_days', 'supportive_comm_days']
+	comm_days_line_chart_data = pd.DataFrame(np.nan, index=multi_index, columns=comm_days_line_chart_cols)
+	comm_vol_line_chart_cols = ['total_comm', 'risky_comm', 'neutral_comm', 'supportive_comm', 'unrated_comm']
+	comm_vol_line_chart_data = pd.DataFrame(np.nan, index=multi_index, columns=comm_vol_line_chart_cols)
 	# print(np.unique(comm_vol_line_chart_data.index.get_level_values('date')))
 
 	report_date = max(date_indices).date()
@@ -74,9 +76,12 @@ def generate_report_data(usernames, date_indices, PROJ_ROOT):
 	for username in usernames:
 		interim_data_file_path = os.path.join(interim_data_path, 'week_comm_log_df_' + username + '.pkl')
 		weekly_comm_df = pd.read_pickle(interim_data_file_path)
+		print(weekly_comm_df.columns)
 
 		comm_pie_chart_data = comm_pie_chart(weekly_comm_df, comm_pie_chart_data, username, report_date - dt.timedelta(7))
+		comm_days_line_chart_data = comm_days_line_chart(weekly_comm_df, comm_days_line_chart_data, username, multi_index)
 		comm_vol_line_chart_data = comm_vol_line_chart(weekly_comm_df, comm_vol_line_chart_data, username, multi_index)
+
 
 	print(comm_vol_line_chart_data.index)
 	print(comm_vol_line_chart_data)
